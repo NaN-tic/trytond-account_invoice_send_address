7e9a4fe3a854180b59fd325df5db53cf0d95dae4
C?dric Krier <ced@b2ck.com>
2018-01-10 14:39:04 +0100

    Remove volumes from drone and fix test

--- setup.py
+++ setup.py
@@ -48,7 +48,8 @@ for dep in info.get('depends', []):
         requires.append(get_require_version('%s_%s' % (prefix, dep)))
 requires.append(get_require_version('trytond'))
 
-tests_require = [get_require_version('proteus')]
+tests_require = [get_require_version('proteus'),
+    get_require_version('trytond_sale')]
 dependency_links = []
 if minor_version % 2:
     # Add development index for testing with proteus
